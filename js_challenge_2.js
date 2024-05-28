60055a6f626eeaec2e4be01ff1dd90df95f4620d
ritikavarghese <728423@sylvaniastudents.org>
2024-05-28 08:57:35 -0400

    js

---
+++ js_challenge_2.js
@@ -0,0 +1,7 @@
+function state() {
+
+        liveIn = prompt("What state do you live in? ");
+        var response = ("You live in ");
+        var period = (".")
+        alert( response + liveIn + period); 
+}
